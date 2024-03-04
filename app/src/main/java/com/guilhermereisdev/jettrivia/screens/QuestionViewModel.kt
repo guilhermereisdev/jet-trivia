5bab06163d56d8d8caec6c0abd4597a580018bf9
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-03-03 23:38:45 -0300

    Cria a ViewModel.

---
+++ app/src/main/java/com/guilhermereisdev/jettrivia/screens/QuestionViewModel.kt
@@ -0,0 +1,32 @@
+package com.guilhermereisdev.jettrivia.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.guilhermereisdev.jettrivia.data.DataOrException
+import com.guilhermereisdev.jettrivia.model.QuestionItem
+import com.guilhermereisdev.jettrivia.repository.QuestionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionViewModel @Inject constructor(
+    private val repository: QuestionRepository
+) : ViewModel() {
+    private val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+        mutableStateOf(DataOrException(null, true, Exception("")))
+
+    init {
+        getAllQuestions()
+    }
+
+    private fun getAllQuestions() {
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repository.getAllQuestions()
+            if (data.value.data.toString().isNotEmpty()) data.value.loading = false
+        }
+    }
+}
