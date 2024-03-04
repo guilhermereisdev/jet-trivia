8306f2060cab405370e8fbcb7196487605ef938c
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-03-03 23:38:27 -0300

    Atualiza a versão do Hilt e adiciona a dependência do Hilt Navigation Compose, que era necessária.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,11 +66,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    //May need okkhttp also
-
-    // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    //May need okkhttp also
     kapt("com.google.dagger:hilt-compiler:2.51")
 
     //Retrofit
